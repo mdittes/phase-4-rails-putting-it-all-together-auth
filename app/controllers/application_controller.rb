89d0fe02a7d3e4020a8bb16fa1003df0160cc383
Matt Dittes <mattdittes@gmail.com>
2022-09-28 15:43:48 -0400

    commit tree b61e97d98b2edfa7422017f384a62c924f237723

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :record_not_valid_response
+  before_action :authorize
+
+  private
+
+  def record_not_valid_response(exception)
+    render json: {errors: exception.record.errors.full_messages}, status: 422
+  end
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+    render json: {errors: ["Not authorized"]}, status: :unauthorized unless @current_user
+  end
 
 end
